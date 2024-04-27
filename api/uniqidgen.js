b67ec8d08087ce82b83b0b8fb00b2dd2051985fb
AdityaKumar41 <119885098+AdityaKumar41@users.noreply.github.com>
2024-04-27 15:24:24 +0530

    add song dev uniqe id

--- api/uniqidgen.js
+++ api/uniqidgen.js
@@ -1,11 +1,28 @@
+let getCall = document.getElementById('getCall');
+let clickBtn = document.getElementById('generateBtn');
+let copyBtn = document.getElementById('copyBtn');
+
 function generateUUID() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-      var r = Math.random() * 16 | 0,
-          v = c === 'x' ? r : (r & 0x3 | 0x8);
-      return v.toString(16);
-  });
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16 | 0,
+            v = c === 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
 }
 
-// Example usage:
-const songId = generateUUID();
-console.log(songId); 
+clickBtn.addEventListener('click', () => {
+    let generatedUUID = generateUUID();
+    getCall.innerText = generatedUUID;
+    getCall.style.display = 'block';
+    copyBtn.style.display = 'block'; 
+});
+
+copyBtn.addEventListener('click', () => {
+    var tempTextArea = document.createElement('textarea');
+    tempTextArea.value = getCall.innerText;
+    document.body.appendChild(tempTextArea);
+    tempTextArea.select();
+    document.execCommand("copy");
+    document.body.removeChild(tempTextArea);
+    alert("Copied to clipboard!");
+});
