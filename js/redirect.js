1b5807b008e0772bf96ec905640b0c682539d5b7
AdityaKumar41 <119885098+AdityaKumar41@users.noreply.github.com>
2024-04-30 22:39:40 +0530

    add google verification

--- js/redirect.js
+++ js/redirect.js
@@ -1,7 +1,7 @@
 function checkScreenWidth() {
   var screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 
-  if (screenWidth < 768) {
+  if (screenWidth < 600) {
       // Redirect
       window.location.href = 'additional/screen.html';
   }
